88f00d17b484d1f877c27700432294cfe4581fe0
top430 <kimqudgus@gmail.com>
2021-09-08 09:25:42 +0900

    1

---
+++ stock/stock/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect
+
+
+
+def index(request):
+    return render(request, 'index.html')
+def main(request):
+    return render(request, 'main.html')
+def inner(request):
+    return render(request, 'inner-page.html')
