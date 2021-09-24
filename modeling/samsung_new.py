e4fca7fcb4066aa5dbb2fe343292654de5b4c9c2
mmeooo <smo8073@naver.com>
2021-09-24 14:59:14 +0900

    update data

--- modeling/samsung_new.py
+++ modeling/samsung_new.py
@@ -1,12 +1,13 @@
-import pandas as pd
 import sqlite3
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
 from pykrx import stock
 import FinanceDataReader as fdr
 from datetime import date, timedelta
 
 conn = sqlite3.connect('samsung.db')  # db파일 생성
 c= conn.cursor()  # db에 연결하는 cursor
-
 
 class KRX:
     def getDate(self):
@@ -16,10 +17,8 @@ class KRX:
     def getStock(self, code):
         self.volume = stock.get_market_ohlcv_by_date(self.start, self.end, code)
         self.volume = self.volume[['거래량']]
-
         self.per = stock.get_market_fundamental_by_date(self.end, self.end, code)
         self.per = self.per[['PER', 'PBR']]
-
         self.value = stock.get_market_trading_value_by_date(self.end, self.end, code)
         self.value = self.value.drop(['전체'], axis=1)
 
@@ -37,10 +36,8 @@ class INVESTING:
     def getStock(self):
         self.sp500 = fdr.DataReader('US500', self.start, self.end)
         self.sp500 = self.sp500[['Close']].rename(columns={'Close':'S&P'})
-
         self.cboe = fdr.DataReader('VIX', self.start, self.end)
         self.cboe = self.cboe[['Close']].rename(columns={'Close':'CBOE'})
-
         self.rate = fdr.DataReader('USD/KRW', self.start, self.end)
         self.rate  = self.rate[['Close']].rename(columns={'Close':'Exchange rate'})
 
@@ -50,6 +47,25 @@ class INVESTING:
         conn.commit()  # db에 저장
 
 
+class INVESTINGCRAWL:
+    def __init__(self):
+        self.contents = []
+
+    def crawling(self, url):
+        self.driver = webdriver.Chrome('C:\chromedriver.exe')
+        self.driver.get(url)
+        self.soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        self.index = self.soup.select('#last_last')
+        self.contents.append([self.index[0].text.strip()])
+        del self.soup
+        self.driver.quit()
+
+    def saving(self):
+        self.df = pd.DataFrame(self.contents, index=['futures2y', 'futures10y']).transpose()
+        self.df.to_sql('samsung', conn, if_exists='replace')
+        conn.commit()
+
+
 if __name__ == "__main__":
     code = "005930"  # 종목코드
     krx = KRX()
@@ -57,10 +73,17 @@ if __name__ == "__main__":
     krx.getStock(code)
     krx.saving()
 
-    invest = INVESTING()
-    invest.getDate()
-    invest.getStock()
-    invest.saving()
+    # invest = INVESTING()
+    # invest.getDate()
+    # invest.getStock()
+    # invest.saving()
+
+    # crawl = INVESTINGCRAWL()
+    # us2yr = 'https://kr.investing.com/rates-bonds/us-2-yr-t-note-historical-data'
+    # crawl.crawling(us2yr)
+    # us10yr = 'https://kr.investing.com/rates-bonds/us-10-yr-t-note-historical-data'
+    # crawl.crawling(us10yr)
+    # crawl.saving()
 
     conn.close()
 
