ba576a9fd236fffe5412fb681e0a46a1c4a86d69
mmeooo <smo8073@naver.com>
2021-09-24 11:56:22 +0900

    update data

--- modeling/samsung_existing.py
+++ modeling/samsung_existing.py
@@ -7,6 +7,5 @@ c= conn.cursor()  # db에 연결하는 cursor
 #c.execute("CREATE TABLE IF NOT EXISTS samsung (id INTEGER PRIMARY KEY AUTOINCREMENT, DATE REAL, 거래량 REAL, PER REAL, PBR REAL, 기관합계 REAL, 기타법인 REAL, 개인 REAL, 외국인합계 REAL)")
 # 내가 연결한 db파일에 TABLE 생성
 
-
 df_excel = pd.read_excel('C:\Develops\mainproject_stock\modeling\data\samsung.xlsx')
 df_excel.to_sql('samsung', conn, if_exists='replace')
