c80009a42652dbaaa26dc9d7f6d2d43c409c49f4
mmeooo <smo8073@naver.com>
2021-09-30 16:12:30 +0900

    finish

--- modeling/samsung_db_schedule.py
+++ modeling/samsung_db_schedule.py
@@ -15,7 +15,7 @@ from datetime import date, datetime, timedelta
 import schedule
 import time
 
-class UPDATE:
+class UpdateDB:
     def __init__(self):
         self.tday = date.today().strftime('%Y%m%d')  # 오늘
         self.yday = (date.today() - timedelta(1)).strftime('%Y%m%d')  # 어제
@@ -23,7 +23,7 @@ class UPDATE:
         self.options.add_argument('headless')
         self.contents = []
 
-    def loadData(self):
+    def loadData(self):  # 기존 데이터 엑셀파일 db에 저장
         try:
             file = pd.read_excel('C:\Develops\mainproject_stock\modeling\data\samsung.xlsx')
         except FileNotFoundError as ex:
@@ -31,18 +31,19 @@ class UPDATE:
         else:
             file.to_sql('samsung', conn, if_exists='append')
 
-    def getKRX(self, code):
+    def getKRX(self, code):  # KRX 데이터
         self.ohlcv = stock.get_market_ohlcv_by_date(self.yday, self.tday, code)[['종가','거래량']]
         self.per = stock.get_market_fundamental_by_date(self.yday, self.tday, code)[['PER', 'PBR']]
         self.value = stock.get_market_trading_value_by_date(self.yday, self.tday, code).drop(['전체'], axis=1)
-        # 개별주관련지표 중 기관합계,기타법인,개인,외국인합계는 데이터 업데이트가 늦음 -> 당일 지표는 당일 16:30?쯤 업데이트 됨
+        # 개별주관련지표 중 기관합계,기타법인,개인,외국인합계는 데이터 업데이트가 늦음
+        # -> 당일 16:30?쯤 업데이트 됨
         df_krx = pd.concat([self.ohlcv, self.per, self.value], axis=1).reset_index()
         df_krx.columns = ['date', 'y', 'volume', 'per', 'pbr', 'institution', 'corp', 'retail', 'foreign']
         self.df_krx = df_krx.set_index('date')
-        print('krxAPI complete.')
+        print('getKRX complete.')
         return self.df_krx
 
-    def getINVEST(self):
+    def getINVEST(self):  # Investing.com 데이터
         self.sp500 = fdr.DataReader('US500', self.yday, self.tday,)[['Close']]
         self.cboe = fdr.DataReader('VIX', self.yday, self.tday,)[['Close']]
         self.rate = fdr.DataReader('USD/KRW', self.yday, self.tday,)[['Close']]
@@ -50,10 +51,10 @@ class UPDATE:
         df_invest = pd.concat([self.sp500, self.cboe, self.rate], axis=1).reset_index()
         df_invest.columns = ['date', 'sp', 'cboe', 'exchangerate']
         self.df_invest = df_invest.set_index('date')
-        print('investingAPI complete.')
+        print('getINVEST complete.')
         return self.df_invest
 
-    def crawlINVEST(self):
+    def crawlINVEST(self):  # Investing.com 크롤링
         self.urls = ['https://kr.investing.com/indices/nasdaq-composite-historical-data','https://kr.investing.com/rates-bonds/us-2-yr-t-note-historical-data', 'https://kr.investing.com/rates-bonds/us-10-yr-t-note-historical-data']
         for url in self.urls:
             self.driver = webdriver.Chrome('C:\chromedriver.exe', options=self.options)
@@ -70,11 +71,10 @@ class UPDATE:
         self.dateindex = pd.DatetimeIndex([datetime.today().date()])
         df_crawl = pd.DataFrame([self.contents], self.dateindex, ['nasdaq','futures2y', 'futures10y']).reset_index().rename(columns={'index': 'date'})
         self.df_crawl = df_crawl.set_index('date')
-        print('crawl complete.')
+        print('crawlINVEST complete.')
         return self.df_crawl
 
-
-    def saving(self):
+    def saving(self):  # 데이터 합쳐서 db에 저장
         tmp = pd.merge(self.df_krx, self.df_invest, on='date')
         self.df_merge = pd.merge(tmp, self.df_crawl, on='date')
         self.df_merge.to_sql('samsung', conn, if_exists='append') # 테이블명
@@ -87,19 +87,17 @@ if __name__ == "__main__":
     conn = sqlite3.connect('samsung.db')
     c = conn.cursor()
 
-    update = UPDATE()  # 클래스 선언
-    #update.loadData()  # 기존 데이터 엑셀파일 db에 저장
+    UpdateDB().loadData()  # 기존 데이터
 
-    t1 = datetime.now()
-    t2 = t1 + timedelta(minutes=1)
-    t3 = t2 + timedelta(minutes=1)
-    t4 = t3 + timedelta(minutes=2)
+    def updateDB():
+        update = UpdateDB()   # 클래스 선언
+        update.getKRX("005930")  # y, volume, per, pbr, institution, corp, retail, foreign
+        update.getINVEST()  # sp, cboe, exchangerate
+        update.crawlINVEST()  # nasdaq, futures2y, futures10y
+        update.saving()  # db에 최종 저장
 
-    schedule.every().day.at(t1.strftime('%H:%M')).do(update.getKRX,'005930')  # y, volume, per, pbr, institution, corp, retail, foreign
-    # schedule.every().day.at(t2.strftime('%H:%M')).do(update.getINVEST())  # sp, cboe, exchangerate
-    # schedule.every().day.at(t3.strftime('%H:%M')).do(update.crawlINVEST())  # nasdaq, futures2y, futures10y
-    # schedule.every().day.at(t4.strftime('%H:%M')).do(update.saving())  # db에 최종 저장
-
+    schedule.every(1).minutes.do(updateDB)  # 1분마다 동작
+    # schedule.every().day.at("5:30").do(updateDB)  # 매일 5:30에 동작
     while True:
         schedule.run_pending()
         time.sleep(1)
