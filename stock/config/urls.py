6699f277e1e4cf9ed485b7adcc057bc23d600cb8
mmeooo <smo8073@naver.com>
2021-10-05 15:38:54 +0900

    뉴스데이터 웹 출력

--- stock/config/urls.py
+++ stock/config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from stock import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('stock.urls'))
+    path('', include('stock.urls')),
+    path('news', views.newsdata),
 ]
--- stock/stock/views.py
+++ stock/stock/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-
+import sqlite3
 
 
 
@@ -16,10 +16,35 @@ def news(request):
 # def inner(request):
 #     return render(request, 'inner-page.html')
 
+# def newsdata(request):
+#     category = request.GET['category'];
+#     date = request.GET['date'];
+#     press = request.GET['press'];
+#     text = request.GET['text'];
+#     title = request.GET['title'];
+#     return render(request, 'news.html');
+
 def newsdata(request):
-    category = request.GET['category'];
-    date = request.GET['date'];
-    press = request.GET['press'];
-    text = request.GET['text'];
-    title = request.GET['title'];
-    return render(request, 'news.html');
+    result = dict()
+    db_news = sqlite3.connect('bigkinds.db')
+    db_news.row_factory = sqlite3.Row
+    c = db_news.cursor()
+
+    # 입력된 기업
+    stockname = request.GET.get('stockname')
+    result['stockname'] = stockname
+
+    # 기업을 다룬 뉴스기사들
+    #c.execute("select title, press, category, date, text from {}_news".format(stockname))
+    c.execute("select title, press, category, date, text from {}_news".format('오리온'))
+    data = c.fetchall()
+    result['erows'] = data
+
+    # 뉴스기사 개수
+    #c.execute("select count(*) from {}_news".format(stockname))
+    c.execute("select count(*) from {}_news".format('오리온'))
+    cnt_curs = c.fetchone()
+    total_cnt =  int(cnt_curs[0])
+    result['total_cnt'] = total_cnt
+
+    return render(request, 'news.html', result)
