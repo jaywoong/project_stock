3df95e824c3b8054f17f45d5581993b82051afc5
jaeung-lee <jaewoong9192@gmail.com>
2021-10-06 09:58:41 +0900

    modify portfolio_model.py for test

--- modeling/portfolio_model.py
+++ modeling/portfolio_model.py
@@ -135,25 +135,36 @@ class OptimizePortfolio:
 
     def custom(self, total_asset, risk_score):  # 설문에서 total_asset 500만원 이상 정수로 받기
         if risk_score <= 20:
-            pf_num = 1
+            pf_num = pf1
         elif risk_score <= 40:
-            pf_num = 2
+            pf_num = pf2
         elif risk_score <= 60:
-            pf_num = 3
+            pf_num = pf3
         elif risk_score <= 80:
-            pf_num = 4
+            pf_num = pf4
         else:
-            pf_num = 5
+            pf_num = pf5
 
         custom_pf = pd.DataFrame(columns=['stock_name', 'num_of_shares', 'amount', 'ratio'])
 
         for i in range(0,len(pf_num)):
             stock_name = pf_num.iloc[i,0]
 
-            conn = sqlite3.connect('./' + stock_name + '.db')
-            c = conn.cursor()
-            stock_data = pd.read_sql('SELECT * FROM ' + stock_name, con=conn)
-            c.close()
+            # conn = sqlite3.connect('./' + stock_name + '.db')
+            # c = conn.cursor()
+            # stock_data = pd.read_sql('SELECT * FROM ' + stock_name, con=conn)
+            # c.close()
+
+# ---------------------------------------------------------------------------------------------------------------------------
+            stock_data = pd.DataFrame({'samsung1':[1,1,1,2,3,4],'lg1':[1,5,6,6,7,7], 'sk1': [1,2,8,12,10,11], 'ls1': [1,13,18,16,20,15],
+                                       'samsung2':[1,1,1,2,3,4],'lg2':[1,5,6,6,7,17], 'sk2': [1,2,8,12,13,11], 'ls2': [1,13,18,8,12,10],
+                                       'samsung3':[1,1,1,2,3,4],'lg3':[1,5,6,16,7,7], 'sk3': [1,2,8,12,16,11], 'ls3': [1,13,18,12,10,20],
+                                       'samsung4':[1,1,1,2,3,4],'lg4':[1,5,6,6,17,7], 'sk4': [1,2,8,12,10,11], 'ls4': [1,13,18,11,20,23],
+                                       'samsung5':[1,1,1,2,3,4],'lg5':[1,5,6,6,7,20], 'sk5': [1,2,8,12,12,11], 'ls5': [1,18,12,10,20,25],
+                                       'samsung6':[1,1,1,2,3,4],'lg6':[1,5,3,6,7,7], 'sk6': [1,2,8,12,17,11], 'ls6': [1,13,1,6,20,18],
+                                       'samsung7':[1,1,1,2,3,4],'lg7':[1,5,6,6,7,8], 'sk7': [1,2,8,12,18,11], 'ls7': [1,1,8,16,20,15],
+                                       'samsung8':[1,1,1,2,3,4],'lg8':[1,5,3,6,7,73500]})
+# ---------------------------------------------------------------------------------------------------------------------------
 
             stock_price = stock_data.iloc[-1,-1]
             stock_buy = total_asset * pf_num.iloc[i,1]
@@ -162,10 +173,14 @@ class OptimizePortfolio:
 
             custom_pf = custom_pf.append({'stock_name' : stock_name , 'num_of_shares' : num_of_shares, 'amount' : amount, 'ratio' : pf1.iloc[i,1]} , ignore_index=True)
 
-        return custom_pf
+        return print(custom_pf)
 
 
 op = OptimizePortfolio()
 op.merge_predicteions()
 op.pf1(merged_data)
-op.custom()
+op.pf2(merged_data)
+op.pf3(merged_data)
+op.pf4(merged_data)
+op.pf5(merged_data)
+op.custom(1000000,55)
